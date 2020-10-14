1e8e5895751710f14f9db7d8d40bf9d7c7485d47
ericgaspar <junk.eg@free.fr>
2020-10-14 12:25:42 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql apt-transport-https"
+pkg_dependencies="postgresql apt-transport-https redis-server"
 
 nodejs_version=12
 
